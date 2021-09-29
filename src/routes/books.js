796ec42cace2a75591e82a4a643cd70d192a7e7a
Juan David Castro Marin <juandah@outlook.com>
2021-09-29 07:25:41 -0500

    se agrega conexión a la bd y  se crea un modelo

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const book = require('../model/book');
 
-router.get('/Libros', (request, response) => {
-
-})
+router.get('/Libros', async(request, response) => 
+{
+    try {
+        let books = await book.find();
+        response.send(books);
+    } catch (error) {
+        response.send('Error: ' + error);
+    }
+        
+});
 
 router.get('/Libros/Crear', (request, response) => {
     response.render('books/create');
