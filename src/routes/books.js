ee7edc18826c6f754c2bc052e48a24ee0ffda5f9
Juan David Castro Marin <juandah@outlook.com>
2021-09-29 07:03:58 -0500

    Revert "General"
    This reverts commit 131b8b82046406822cbe5043ea585f571198ad93.

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/Libros', (request, response) => {
 
+})
+
+router.get('/Libros/Crear', (request, response) => {
+    response.render('books/create');
+})
 
 module.exports= router;
