6bab718d4681ffd307fc501cf9c9d3512c177a99
Juan David Castro Marin <juandah@outlook.com>
2021-10-07 11:12:54 -0500

    Correcion de conxión a la db

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
  var mongoose = require('mongoose');
- const uriAtalas = 'mongodb+srv://admin:Admin123@clusterlibreria.1lv3v.mongodb.net/DbLibreria?retryWrites=true&w=majority;'
+ const uriAtalas = 'mongodb+srv://admin:Admin123@clusterlibreria.1lv3v.mongodb.net/DBLibreria?authSource=admin&replicaSet=atlas-lhwkid-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
 const uriLocal= 'mongodb://localhost/DBLibreria'
 
- mongoose.connect('uriAtalas', 
+ mongoose.connect( uriAtalas, 
                   {  connectTimeoutMS: 3000
                      , serverSelectionTimeoutMS: 3000
                   })
