77971848bc8d3d46520842aa41f4525c6b830637
Juan David Castro Marin <juandah@outlook.com>
2021-10-07 10:54:17 -0500

    Se organizan vistas
    Se organizan vistas y se colca uri de Atlas

--- src/database.js
+++ src/database.js
@@ -1,15 +1,10 @@
  var mongoose = require('mongoose');
+ const uriAtalas = 'mongodb+srv://admin:Admin123@clusterlibreria.1lv3v.mongodb.net/DbLibreria?retryWrites=true&w=majority;'
+const uriLocal= 'mongodb://localhost/DBLibreria'
 
- mongoose.connect('mongodb+srv://admin:Admin123@clusterlibreria.1lv3v.mongodb.net/DBLibreria?authSource=admin&replicaSet=atlas-lhwkid-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', 
+ mongoose.connect('uriAtalas', 
                   {  connectTimeoutMS: 3000
                      , serverSelectionTimeoutMS: 3000
                   })
             .then(bd => console.log('Base de datos connectada'))
             .catch(error => console.error('Error al conectarse con la base de datos: ' + error));
-
-// mongoose.connect('mongodb://localhost/DBLibreria', 
-//       {  connectTimeoutMS: 3000
-//          , serverSelectionTimeoutMS: 3000
-//       })
-// .then(bd => console.log('Base de datos connectada'))
-// .catch(error => console.error('Error al conectarse con la base de datos: ' + error));
